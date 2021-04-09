eaee3b7458477b25e78efeca8fe3135b5824744d
andrzej.baran91@gmail.com <hehe2razy>
2021-04-09 09:58:15 +0200

    Spring boot project initialized with: DevTools, JPA, MySQL, Security, Thymeleaf, Web

---
+++ src/main/java/com/IntranetForFun/IntranetForFunApplication.java
@@ -0,0 +1,13 @@
+package com.IntranetForFun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntranetForFunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntranetForFunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IntranetForFun/IntranetForFunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IntranetForFun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntranetForFunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
